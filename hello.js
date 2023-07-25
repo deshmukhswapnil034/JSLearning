38d52c8437634e23bb886a73cefbaf71b729335a
deshmukhswapnil034 <deshmukhswapnil034@gmail.com>
2023-07-25 16:34:02 -0400

    opretor.js

--- hello.js
+++ hello.js
@@ -1,1 +1,19 @@
-console.log("hello word");
+var firstName = "swapnil";
+console.log(firstName);
+console.log(typeof firstName);
+
+var age = 23;
+console.log(age);
+console.log(typeof age);
+
+var isMarried = true;
+console.log(isMarried);
+console.log(typeof isMarried);
+
+var city = 'Pune';
+console.log(typeof city);
+city = 50000;
+console.log(typeof city);
+
+
+console.log("city");
